161deaf600ddbd3f2137dc01632e5142ca1a0f5e
etoledom <etoledom@icloud.com>
2017-12-13 12:48:06 -0300

    Initial Commit

---
+++ Weatherfy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weatherfy
+//
+//  Created by Eduardo Toledo on 12/13/17.
+//  Copyright © 2017 SoSafe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Weatherfy/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Weatherfy
+//
+//  Created by Eduardo Toledo on 12/13/17.
+//  Copyright © 2017 SoSafe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
